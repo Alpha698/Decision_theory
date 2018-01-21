db149960a2ab031a5992fb45c5ebd59db13d133c
Alpha698 <tsef47@mail.ru>
2018-01-21 22:00:12 +0200

    Update_v7.3

---
+++ Lab_8/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(390, 245);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(367, 188);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "B1";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 80;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "B2";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 80;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "B3";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 80;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "B4";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 80;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(398, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Bi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 212);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ai";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(41, 209);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(80, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(127, 209);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(80, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(213, 209);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(80, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(299, 209);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(80, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(385, 38);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(80, 20);
+            this.textBox5.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(385, 64);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(80, 20);
+            this.textBox6.TabIndex = 13;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(385, 90);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(80, 20);
+            this.textBox7.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(431, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "_";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(431, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "_";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(481, 281);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
+            this.Name = "Form1";
+            this.Text = "Теория игр";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Lab_8/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            double perv1 = 8; double perv2 = 6; double perv3 = 2; double perv4 = 4;
+            double vto1 = 8; double vto2 = 9; double vto3 = 4; double vto4 = 5; 
+            double tre1 = 7; double tre2 = 5; double tre3 = 3; double tre4 = 5; 
+
+            dataGridView1.Rows.Add(perv1, perv2, perv3, perv4);
+            dataGridView1.Rows.Add(vto1, vto2, vto3, vto4);
+            dataGridView1.Rows.Add(tre1, tre2, tre3, tre4);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String s1 = dataGridView1[0, 0].Value.ToString();
+            double as1 = Convert.ToDouble(s1);
+            String s2 = dataGridView1[1, 0].Value.ToString();
+            double as2 = Convert.ToDouble(s2);
+            String s3 = dataGridView1[2, 0].Value.ToString();
+            double as3 = Convert.ToDouble(s3);
+            String s4 = dataGridView1[3, 0].Value.ToString();
+            double as4 = Convert.ToDouble(s4);
+
+            double[] array = { as1, as2, as3, as4 };
+            double min = double.MaxValue;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                    min = array[i];
+            }
+            textBox5.Text = min.ToString();
+            ///////////////////////////////////////////////////////
+            String a1 = dataGridView1[0, 1].Value.ToString();
+            double aa1 = Convert.ToDouble(a1);
+            String a2 = dataGridView1[1, 1].Value.ToString();
+            double aa2 = Convert.ToDouble(a2);
+            String a3 = dataGridView1[2, 1].Value.ToString();
+            double aa3 = Convert.ToDouble(a3);
+            String a4 = dataGridView1[3, 1].Value.ToString();
+            double aa4 = Convert.ToDouble(a4);
+
+            double[] arraya = { aa1, aa2, aa3, aa4 };
+            double mina = double.MaxValue;
+            for (int i = 0; i < arraya.Length; i++)
+            {
+                if (arraya[i] < mina)
+                    mina = arraya[i];
+            }
+            textBox6.Text = mina.ToString();
+            ///////////////////////////////////////////////////////
+            String b1 = dataGridView1[0, 2].Value.ToString();
+            double ab1 = Convert.ToDouble(b1);
+            String b2 = dataGridView1[1, 2].Value.ToString();
+            double ab2 = Convert.ToDouble(b2);
+            String b3 = dataGridView1[2, 2].Value.ToString();
+            double ab3 = Convert.ToDouble(b3);
+            String b4 = dataGridView1[3, 2].Value.ToString();
+            double ab4 = Convert.ToDouble(b4);
+
+            double[] arrayb = { ab1, ab2, ab3, ab4 };
+            double minb = double.MaxValue;
+            for (int i = 0; i < arrayb.Length; i++)
+            {
+                if (arrayb[i] < minb)
+                    minb = arrayb[i];
+            }
+            textBox7.Text = minb.ToString();
+            ///////////////////////////////////////////////////////
+            ///MAX B
+            ///////////////////////////////////////////////////////
+            double[] arrays = { min, mina, minb };
+            double maxs = double.MinValue;
+            for (int i = 0; i < arrays.Length; i++)
+            {
+                if (arrays[i] > maxs)
+                    maxs = arrays[i];
+            }
+            label4.Text = maxs.ToString();
+            ///////////////////////////////////////////////////////////////////////////////
+            String c1 = dataGridView1[0, 0].Value.ToString();
+            double ac1 = Convert.ToDouble(c1);
+            String c2 = dataGridView1[0, 1].Value.ToString();
+            double ac2 = Convert.ToDouble(c2);
+            String c3 = dataGridView1[0, 2].Value.ToString();
+            double ac3 = Convert.ToDouble(c3);
+
+            double[] arrayc = { ac1, ac2, ac3 };
+            double minc = double.MaxValue;
+            for (int i = 0; i < arrayc.Length; i++)
+            {
+                if (arrayc[i] < minc)
+                    minc = arrayc[i];
+            }
+            textBox1.Text = minc.ToString();
+            ///////////////////////////////////////////////////////
+            String d1 = dataGridView1[1, 0].Value.ToString();
+            double ad1 = Convert.ToDouble(d1);
+            String d2 = dataGridView1[1, 1].Value.ToString();
+            double ad2 = Convert.ToDouble(d2);
+            String d3 = dataGridView1[1, 2].Value.ToString();
+            double ad3 = Convert.ToDouble(d3);
+
+            double[] arrayd = { ad1, ad2, ad3 };
+            double mind = double.MaxValue;
+            for (int i = 0; i < arrayd.Length; i++)
+            {
+                if (arrayd[i] < mind)
+                    mind = arrayd[i];
+            }
+            textBox2.Text = mind.ToString();
+            ///////////////////////////////////////////////////////
+            String e1 = dataGridView1[2, 0].Value.ToString();
+            double ae1 = Convert.ToDouble(e1);
+            String e2 = dataGridView1[2, 1].Value.ToString();
+            double ae2 = Convert.ToDouble(e2);
+            String e3 = dataGridView1[2, 2].Value.ToString();
+            double ae3 = Convert.ToDouble(e3);
+
+            double[] arraye = { ae1, ae2, ae3};
+            double mine = double.MaxValue;
+            for (int i = 0; i < arraye.Length; i++)
+            {
+                if (arraye[i] < mine)
+                    mine = arraye[i];
+            }
+            textBox3.Text = mine.ToString();
+            ///////////////////////////////////////////////////////
+            String f1 = dataGridView1[3, 0].Value.ToString();
+            double af1 = Convert.ToDouble(f1);
+            String f2 = dataGridView1[3, 1].Value.ToString();
+            double af2 = Convert.ToDouble(f2);
+            String f3 = dataGridView1[3, 2].Value.ToString();
+            double af3 = Convert.ToDouble(f3);
+
+            double[] arrayf = { af1, af2, af3 };
+            double minf = double.MaxValue;
+            for (int i = 0; i < arrayf.Length; i++)
+            {
+                if (arrayf[i] < minf)
+                    minf = arrayf[i];
+            }
+            textBox4.Text = minf.ToString();
+            ///////////////////////////////////////////////////////
+            ///MAX A
+            ///////////////////////////////////////////////////////
+            double[] arrayss = { minc, mind, mine,minf };
+            double maxss = double.MinValue;
+            for (int i = 0; i < arrayss.Length; i++)
+            {
+                if (arrayss[i] > maxss)
+                    maxss = arrayss[i];
+            }
+            label3.Text = maxss.ToString();
+            ///////////////////////////////////////////////////////
+
+        }
+    }
+}
